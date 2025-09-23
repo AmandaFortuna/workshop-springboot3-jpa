d4c8e4d8abcf9e7b4374a4c75860f82dadec8119
AmandaFortuna <amanda.fortuna.santos@hotmail.com>
2025-09-23 12:42:56 -0300

    Service layer, component registration

--- src/main/java/com/amandafortuna/course/resources/UserResource.java
+++ src/main/java/com/amandafortuna/course/resources/UserResource.java
@@ -1,20 +1,34 @@
 package com.amandafortuna.course.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.amandafortuna.course.entities.User;
+import com.amandafortuna.course.services.UserService;
 
 @RestController
-@RequestMapping(value= "/users")
+@RequestMapping(value= "/users/")
 public class UserResource {
 
+	@Autowired
+	private UserService service;
+	
 	@GetMapping
-	public ResponseEntity<User> finsAll() {
-		User u = new User(1L, "MAria", "d@mmmm", "198717163553", "GFDER22");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> finsAll() {
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
 	}
 	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+		
+	}
 }
